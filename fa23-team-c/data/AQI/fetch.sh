9f140ce69c72fbf154aa0e631c0c62cccd2eaad6
Eric Wang <eric.wang2237@gmail.com>
2023-10-27 19:37:15 -0400

    Adding/organizing raw data

--- fa23-team-c/data/AQI/fetch.sh
+++ fa23-team-c/data/AQI/fetch.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-for x in {20210101..20211231}; do
+for x in {20211201..20211231}; do
     wget "https://s3-us-west-1.amazonaws.com/files.airnowtech.org/airnow/2021/$x/HourlyAQObs_${x}00.dat"
 done
