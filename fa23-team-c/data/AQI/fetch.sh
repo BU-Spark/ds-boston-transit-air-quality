b079036e477d0648ad52bc72a9a16898b2042b85
Eric Wang <eric.wang2237@gmail.com>
2023-10-28 19:30:51 -0400

    Adding monthly AQI data from 2014 to present day

--- fa23-team-c/data/AQI/fetch.sh
+++ fa23-team-c/data/AQI/fetch.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
-for x in {20211201..20211231}; do
-    wget "https://s3-us-west-1.amazonaws.com/files.airnowtech.org/airnow/2021/$x/HourlyAQObs_${x}00.dat"
+#https://files.airnowtech.org/?prefix=airnow/2014/20140101/
+
+
+for x in {2020..2023}; do
+    # echo $x
+
+    for y in {1..9}; do
+        # echo $y
+        wget "https://s3-us-west-1.amazonaws.com/files.airnowtech.org/airnow/$x/${x}0${y}01/HourlyAQObs_${x}0${y}0100.dat"
+    done
+
+    for y in {10..12}; do
+        # echo $y
+        wget "https://s3-us-west-1.amazonaws.com/files.airnowtech.org/airnow/$x/${x}${y}01/HourlyAQObs_${x}${y}0100.dat"
+    done
+
 done
